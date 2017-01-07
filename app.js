0ff6e30a39523ee644651c30e31a2daceeaf4ab0
Zi-Yuan Chan <ziyuan@zynick.com>
2017-01-07 15:04:06 +0800

    switch from jade to pug

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ module.exports = (serial, mqtt) => {
 
     /* View Engine Setup */
     app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'jade');
+    app.set('view engine', 'pug');
 
 
     /* App Stacks */
